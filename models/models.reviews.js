e51bda3160d8b4db76fe6d9a0e6dc6ab931ee51c
reawilliams20 <Andrea.NgokaWilliams@kpmg.co.uk>
2022-12-12 14:50:50 +0000

    updated 4-get-reviews comment_count query

--- models/models.reviews.js
+++ models/models.reviews.js
@@ -2,8 +2,9 @@ const db = require("../db/connection");
 
 exports.selectReviews = () => {
     return db.query(`
-    SELECT review_id, title, designer, owner, review_img_url, review_body, category, created_at, votes, COUNT(review_id) AS comment_count FROM reviews
-    GROUP BY review_id
+    SELECT reviews.review_id, title, designer, owner, review_img_url, review_body, category, reviews.created_at, reviews.votes, COUNT(comments.review_id) AS comment_count FROM reviews
+    FULL OUTER JOIN comments ON comments.review_id = reviews.review_id
+    GROUP BY reviews.review_id
     ORDER BY created_at DESC;`)
     .then((result) => {
         return result.rows;
