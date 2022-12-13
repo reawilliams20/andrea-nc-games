4d2c5d484321b2ae6e673d738bb053bf4b8b2b7d
reawilliams20 <Andrea.NgokaWilliams@kpmg.co.uk>
2022-12-13 11:55:01 +0000

    changed location of error handling files, put into one file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {getCategories} = require("./controllers/controllers.categories")
 
 const {getReviews, getReview} = require("./controllers/controllers.reviews");
 
-const {handle404Routes} = require("./controllers/controllers.errors")
+const {handle404Routes, handle400Error, handle500Error} = require("./error-handling")
 
 const app = express();
 
@@ -13,26 +13,10 @@ app.get('/api/categories', getCategories);
 app.get('/api/reviews', getReviews);
 app.get('/api/reviews/:review_id', getReview);
 
+
+app.use(handle400Error);
+app.use(handle500Error);
 app.all('*', handle404Routes);
 
-//handles sql query error
-app.use((err, req, res, next) => {
-    if (err.code === '22P02'){
-        res.status(400).send({msg: 'bad request'});
-    }
-    else{
-        next(err);
-    }
-});
-
-//handles promise reject to send back status and message
-app.use((err, req, res, next) => {
-    if(err.msg !== undefined){
-        res.status(err.status).send({msg: err.msg});
-    }
-    else{
-        next(err);
-    }
-});
 
 module.exports = app;
