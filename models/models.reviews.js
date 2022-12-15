11098b53eeed52ed68abd3cff57318a62a4c73cd
reawilliams20 <Andrea.NgokaWilliams@kpmg.co.uk>
2022-12-15 14:05:47 +0000

    get-reviews-queries completed

--- models/models.reviews.js
+++ models/models.reviews.js
@@ -1,11 +1,32 @@
 const db = require("../db/connection");
 
-exports.selectReviews = () => {
-    return db.query(`
+exports.selectReviews = (category, sort_by="created_at", order="desc") => {
+    const validOrderQueries = ['asc', 'desc']
+    const validSortByQueries = ['title', 'designer', 'owner', 'review_img_url', 'category', 'created_at', 'votes', 'review_id']
+    const validCategoryQueries = ['euro game', 'social deduction', 'dexterity', "children's games"]
+
+    if(!validSortByQueries.includes(sort_by) || !validOrderQueries.includes(order)) {
+        return Promise.reject({status: 400, msg: 'bad request'});
+    }
+
+    let queryString = `
     SELECT reviews.review_id, title, designer, owner, review_img_url, review_body, category, reviews.created_at, reviews.votes, COUNT(comments.review_id) AS comment_count FROM reviews
-    LEFT JOIN comments ON comments.review_id = reviews.review_id
-    GROUP BY reviews.review_id
-    ORDER BY created_at DESC;`)
+    LEFT JOIN comments ON comments.review_id = reviews.review_id `
+
+    const queryArr = []
+    if (category !== undefined) {
+        if (!validCategoryQueries.includes(category)) {
+            return Promise.reject({status: 400, msg: 'bad request'})
+        } else {
+            queryString += ` WHERE category = $1 `
+            queryArr.push(category)
+        }
+    }
+    
+    queryString += `GROUP BY reviews.review_id 
+    ORDER BY ${sort_by} ${order};`
+
+    return db.query(queryString, queryArr)
     .then((result) => {
         return result.rows;
     })
