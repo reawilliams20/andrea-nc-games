53dd9091b7bf1469dcc59837414be1f3914fb950
reawilliams20 <Andrea.NgokaWilliams@kpmg.co.uk>
2023-01-11 15:12:08 +0000

    update category queries

--- models/models.reviews.js
+++ models/models.reviews.js
@@ -3,8 +3,7 @@ const db = require("../db/connection");
 exports.selectReviews = (category, sort_by="created_at", order="desc") => {
     const validOrderQueries = ['asc', 'desc']
     const validSortByQueries = ['title', 'designer', 'owner', 'review_img_url', 'category', 'created_at', 'votes', 'review_id', 'comment_count']
-    const validCategoryQueries = ['euro game', 'social deduction', 'dexterity', "children's games"]
-
+    const validCategoryQueries = ["euro game", "dexterity", "social deduction", "children's games", "strategy", "hidden-roles", "push-your-luck", "roll-and-write", "deck-building", "engine-building"]
     if(!validSortByQueries.includes(sort_by) || !validOrderQueries.includes(order)) {
         return Promise.reject({status: 400, msg: 'bad request'});
     }
